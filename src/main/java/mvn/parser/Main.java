18b6f91e2ecddf48177fb9ff97f689dcb9bd4a29
Vadim_K <vnkushchenko@gmail.com>
2023-01-16 15:47:39 +0300

    Initial commit

---
+++ src/main/java/mvn/parser/Main.java
@@ -0,0 +1,64 @@
+package mvn.parser;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        list.forEach(System.out::println);
+
+        String json = listToJson(list);
+        System.out.println(json);
+
+        createJson(json);
+    }// main
+
+    public static List<Employee> parseCSV(String[] columhMapping, String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new FileReader("data.csv"))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columhMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            list = csv.parse();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return list;
+    }
+
+    public static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.toJson(list, listType);
+    }
+
+    public static void createJson(String input) {
+        try (FileWriter writer = new FileWriter("data.json", false)) {
+            writer.write(input);
+            writer.flush();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}// class
